a493231bdfaf665e8ac929f3bb545173ca00ec50
ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>
2022-08-21 15:22:45 +0300

    api-gateway entegrasyonu yapıldı

---
+++ gateway-server/src/main/java/com/ticket/server/HystrixController.java
@@ -0,0 +1,20 @@
+package com.ticket.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/feedback")
+public class HystrixController {
+    @GetMapping("/account")
+    public String accountFallback(){
+        return "Account Service is not available.";
+    }
+
+    @GetMapping("/ticket")
+    public String ticketFallback(){
+        return "Ticket Service is not available.";
+    }
+
+}
